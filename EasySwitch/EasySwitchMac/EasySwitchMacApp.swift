dc6165a8c6d2fb59f56469e7f3c2d81f0f8355b1
Vishek Tyagi <tyagivishek1234@gmail.com>
2026-03-17 11:47:54 +0530

    updated for macOS

---
+++ EasySwitch/EasySwitchMac/EasySwitchMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasySwitchMacApp.swift
+//  EasySwitchMac
+//
+//  Created by vishek on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct EasySwitchMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ EasySwitch/EasySwitchMacTests/EasySwitchMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasySwitchMacTests.swift
+//  EasySwitchMacTests
+//
+//  Created by vishek on 17/03/26.
+//
+
+import Testing
+@testable import EasySwitchMac
+
+struct EasySwitchMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
